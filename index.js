263be5d59956cb11027d5cc2952bab403a7dbde7
Marko <marko.pejcic12321@gmail.com>
2024-02-18 13:28:11 +0200

    Updated Landing Page

--- index.js
+++
@@ -1,9 +0,0 @@
-
-const myBoolean = true;
-const myString = "hello world"
-const firstNumber=20;
-let secondNumber=40;
-secondNumber=80;
-const myarray = [ myBoolean,myString]
-console.log(myarray[0]);
-console.log(secondNumber)
